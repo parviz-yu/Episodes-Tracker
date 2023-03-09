0b53911ccf6c7ff1bad23bea98f7ff84fea84a8a
pyuldashev912 <www.parviz98@ya.ru>
2023-03-09 17:39:47 +0500

    Added consumer and app's entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/pyuldashev912/tracker/internal/client"
+	"github.com/pyuldashev912/tracker/internal/consumer"
+	"github.com/pyuldashev912/tracker/internal/events/telegram"
+	"github.com/pyuldashev912/tracker/internal/storage/sqlite"
+)
+
+func main() {
+	host := "api.telegram.org"
+	token := os.Getenv("BOT_TOKEN")
+	storage_path := "data/sqlite"
+
+	cli := client.New(host, token)
+	storage, err := sqlite.New(storage_path, "/storage.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	storage.Init()
+
+	eventProcessor := telegram.New(cli, storage)
+	c := consumer.New(eventProcessor, eventProcessor, 100)
+	fmt.Println("Service started...")
+
+	if err := c.CheckToken(token); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Token is valid")
+	c.Start()
 }
